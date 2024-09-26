46b31a3a56b5bc7fe14c9d34a9cdb004c75519cf
ParitoshAnilNeelamChaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-09-27 02:34:10 +0530

    Create 3-1-counting-bits-using-popcount-function.cpp

---
+++ 3-1-counting-bits-using-popcount-function.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/counting-bits/description/
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans ;
+
+        for(int i = 0 ; i <= n ; ++i)
+        {
+            int x = __builtin_popcount(i) ;
+            ans.push_back(x);
+        }
+        return ans ;
+    }
+};
