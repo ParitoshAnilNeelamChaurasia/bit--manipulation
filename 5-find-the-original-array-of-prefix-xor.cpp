301939e845cc14f9fdc74c2687ed975d4db223be
ParitoshAnilNeelamChaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-09-27 17:04:42 +0530

    Create 5-find-the-original-array-of-prefix-xor.cpp

---
+++ 5-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int n = pref.size() ;
+
+        vector<int> ans(n) ;
+
+        ans[0] = pref[0] ;
+
+        for(int i = 1 ; i < n ; ++i)
+        {
+            ans[i] = pref[i-1] ^ pref[i] ; 
+        }
+
+        return ans ;
+    }
+};
